6738b1427ca8ea85cd59f270e85e77d792c121eb
H. Thomson Comer <tcomer@cardinalpeak.com>
2012-11-01 08:10:49 -0600

    Set new Ev types in Settings class

--- advmodel/AdvEvents.py
+++ advmodel/AdvEvents.py
@@ -2,12 +2,12 @@
 # This module contains all of the event classes and their defaults
 
 class Event:
-   def __init__():
+   def __init__(self):
       self.next = None
       self.prev = None
 
 class WelcomeEv(Event):
-   def __init__():
+   def __init__(self):
       self.welcome = "Welcome to %s!  You have previously braved upon adventure!"
       self.select = "S)elect, C)reate, or D)elete a character"
       self.opts = ["s","c","d"]
@@ -15,39 +15,39 @@ class WelcomeEv(Event):
       self.toomanytocreate = "You have enough characters.  Finish the game with one or delete it!"
 
 class StandingsEv(Event):
-   def __init__():
+   def __init__(self):
       self.title = "%s Standings"
       self.select = "T)op 10    M)y rank    C)lasses"
       self.opts = ["t","m","c"]
 
 class ShopEv(Event):
-   def __init__():
+   def __init__(self):
       self.title = "Welcome to %s's store!  What would you like to buy?"
       self.select = "W)eapon, A)rmor, or I)tem: "
       self.opts = ["w","a","i"]
 
 class SellItemEv(Event):
-   def __init__():
+   def __init__(self):
       self.title = "Lighten Your LoadI"
       self.select = "Select an item to sell (n): "
       self.confirm = "Are you sure you want to sell %s for %dg?"
       self.done = "%s sold!"
 
 class RewardEv(Event):
-   def __init__():
+   def __init__(self):
       self.title = "You received:"
 
 class QuitEv(Event):
-   def __init__():
+   def __init__(self):
       self.title = "%s is shutting down.  Thanks for playing and come back soon!"
 
 class PlayerMakeEv(Event):
-   def __init__():
+   def __init__(self):
       self.entername = "Enter your name: "
       self.chooseclass = "Choose your class: "
 
 class PlayerInfoEv(Event):
-   def __init__():
+   def __init__(self):
       self.select = "D)one"
       self.inventory = "C)haracter inventory"
       self.achievements = "P)layer achievements"
@@ -55,18 +55,18 @@ class PlayerInfoEv(Event):
       self.opts = ["d","c","p","s"]
 
 class DecideEv(Event):
-   def __init__():
+   def __init__(self):
       self.title = "You must decide."
 
 class BuyItemEv(Event):
-   def __init__():
+   def __init__(self):
       self.title = "You bought %s!"
       self.equipreminder = "Don't forget to equip it!"
       self.usereminder = "You can use it in battle for an edge."
       self.noncombat = "This item can only be used outside of combat."
 
 class AchievementsEv(Event):
-   def __init__():
+   def __init__(self):
       self.title = "You are a well seasoned berserker!"
       self.subtitle = "Achievements received: "
       self.total = "%d/%d achievements unlocked!"
--- advmodel/Settings.py
+++ advmodel/Settings.py
@@ -1,5 +1,48 @@
 # The Settings class contains all of the defaults required
 # for prompting the user.
 
+from AdvEvents import *
+
 class Settings:
-   pass
+   def __init__(self):
+      self.gamename = "default"
+      self.anykey = "anykey"
+      self.none = "none"
+      self.basics = None
+      self.globalmenu = None
+      self.WelcomeEv = WelcomeEv()
+      self.StandingsEv = StandingsEv()
+      self.ShopEv = ShopEv()
+      self.SellItemEv = SellItemEv()
+      self.RewardEv = RewardEv()
+      self.QuitEv = QuitEv()
+      self.PlayerMakeEv = PlayerMakeEv()
+      self.PlayerInfoEv = PlayerInfoEv()
+      self.DecideEv = DecideEv()
+      self.BuyItemEv = BuyItemEv()
+      self.AchievementsEv = AchievementsEv()
+   def __str__(self):
+      return """
+   Adv Localized Settings Loaded:
+      Game-Name: %s
+      AnyKey Prompt: %s
+      None: %s
+      Basics Menu: %s
+      Global Menu: %s
+      
+      WelcomeEv: %s
+      StandingsEv: %s 
+      ShopEv: %s
+      SellItemEv: %s
+      RewardEv: %s
+      QuitEv: %s
+      PlayerMakeEv: %s
+      PlayerInfoEv: %s
+      DecideEv: %s
+      BuyItemEv: %s
+      AchievementsEv: %s""" % (self.gamename,self.anykey,self.none,self.basics,self.globalmenu, \
+                        self.WelcomeEv,self.StandingsEv,self.ShopEv,self.SellItemEv, \
+                        self.RewardEv,self.QuitEv,self.PlayerMakeEv,self.PlayerInfoEv, \
+                        self.DecideEv,self.BuyItemEv,self.AchievementsEv)
+   def __repr__(self):
+      return self.__str__()
